794fe47630cd63fee96665a0cbfcf49af9ac21f0
Wuangg <52225998+Duc-Nguyen98@users.noreply.github.com>
2026-09-10 08:48:53 +0700

    chore: clear repository for design image uploads

--- components/warranty-scanner-app.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import ScannerPreview from './scanner-preview';
-import ScannerAppShell from './scanner-app-shell';
-
-export default function WarrantyScannerApp() {
-  return <ScannerAppShell><ScannerPreview /></ScannerAppShell>;
-}
--- main.tsx
+++
@@ -1,6 +0,0 @@
-import {createRoot} from 'react-dom/client';
-import './styles/fonts.css';
-import './styles/base.css';
-import WarrantyScannerApp from './components/warranty-scanner-app';
-
-createRoot(document.getElementById('root')!).render(<WarrantyScannerApp />);
